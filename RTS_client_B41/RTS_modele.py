60932f1b960947b8a4470d8eb7f5bd09432a6838
PoulpePalpitant <64504737+PoulpePalpitant@users.noreply.github.com>
2021-04-21 16:30:41 -0400

    brrp

--- RTS_client_B41/RTS_modele.py
+++ RTS_client_B41/RTS_modele.py
@@ -6,40 +6,6 @@ from helper import Helper
 from RTS_divers import *
 import math
 
-class UpgradeTest():
-    def __init__(self, param):
-        name, description, productionTime, cost, prerequisites, img = param    # Les arguments doivent être passé dans cet ordre là
-
-        self.name = name
-        self.description = description
-        self.productionTime = productionTime    # Temps que ça prend pour le faire
-        self.cost = cost                        
-        self.prerequisites = prerequisites      # La liste des upgrades qui doivent être fait pour débloquer celui-ci
-        self.img = img                          # Icone sur le bouton de l'upgrade
-
-    def effect(self, player):   # Définis dans les sous-classes
-        return
-
-
-class ProteinShake():
-    def __init__(self):
-        UpgradeTest.__init__(self, # Parent constructor
-        ["Protein shakes",
-         "Le pouvoir des protéines : Augmente la quantité de ressources que peut transporter tout les slaves 50%", # Une description        
-        None,   # Time
-        None,   # Cost
-        None,   # Prerequesite
-        None])   # Icon    
-
-    def effect(self, player): # Effet de l'upgrade sur le joueur/partie
-        slaveList = player.persos['ouvrier']
-        for slave in slaveList: 
-            slaveList[slave].quota += slaveList[slave].quota / 2
-        
-class UpgradeRegistry():
-    UPGRADES = {
-        "Protein shakes":ProteinShake(),
-    }
 
 
 
@@ -617,8 +583,6 @@ class Chevalier(Perso):
         self.atkDmg = 15
         self.atkSpeed = 2
         self.attackTimer.set(self.atkSpeed) # Obligatoire si unité peut attaquer
-
-
 
 class Druide(Perso):
     def __init__(self,parent,id,maison,couleur,x,y,montype):
